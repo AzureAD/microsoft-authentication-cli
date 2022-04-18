1610ad9436a308f3ae083ff59c861ddbf0f430dc
Kyle Rader <kyrader@microsoft.com>
2022-04-18 13:40:41 -0700

    Fix up StyleCop configuration for test projects

--- src/stylecop/GlobalSuppressions.Test.cs
+++ src/stylecop/GlobalSuppressions.Test.cs
@@ -3,5 +3,5 @@
 
 using System.Diagnostics.CodeAnalysis;
 
+[assembly: SuppressMessage("StyleCop.CSharp.SpecialRules", "SA0001: XML comment analysis is disabled due to project configuration", Justification = "Not needed in test project.")]
 [assembly: SuppressMessage("StyleCop.CSharp.OrderingRules", "SA1600: Elements should be documented", Justification = "Not needed in test project.")]
-[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "CS1591: Missing XML comment for publicly visible type or member", Justification = "Not needed in test project.")]
