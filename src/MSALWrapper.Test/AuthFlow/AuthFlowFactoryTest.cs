7b7cc45319338cedf79c7ff0747ecd74395af1ea
Kyle Rader <kyrader@microsoft.com>
2022-04-22 17:46:47 -0700

    Implement initial test for getting only a broker

--- src/MSALWrapper.Test/AuthFlow/AuthFlowFactoryTest.cs
+++ src/MSALWrapper.Test/AuthFlow/AuthFlowFactoryTest.cs
@@ -3,6 +3,7 @@
 
 namespace MSALWrapper.Test.AuthFlow
 {
+    using System;
     using System.Collections.Generic;
     using System.Linq;
 
@@ -10,15 +11,77 @@ namespace MSALWrapper.Test.AuthFlow
 
     using Microsoft.Authentication.MSALWrapper;
     using Microsoft.Authentication.MSALWrapper.AuthFlow;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Logging;
+
+    using Moq;
+
+    using NLog.Extensions.Logging;
+    using NLog.Targets;
 
     using NUnit.Framework;
 
     internal class AuthFlowFactoryTest
     {
+        private static readonly Guid ResourceId = new Guid("6e979987-a7c8-4604-9b37-e51f06f08f1a");
+        private static readonly Guid ClientId = new Guid("5af6def2-05ec-4cab-b9aa-323d75b5df40");
+        private static readonly Guid TenantId = new Guid("8254f6f7-a09f-4752-8bd6-391adc3b912e");
+
+        private MemoryTarget logTarget;
+        private ServiceProvider serviceProvider;
+        private Mock<IPCAWrapper> pcaWrapperMock;
+        private ILogger logger;
+        private AuthMode authMode;
+        private IEnumerable<string> scopes;
+        private string osxKeyChainSuffix;
+        private string preferredDomain;
+        private IPCAWrapper pcaWrapper;
+        private string promptHint;
+
+        [SetUp]
+        public void Setup()
+        {
+            // Setup in memory logging target with NLog - allows making assertions against what has been logged.
+            var loggingConfig = new NLog.Config.LoggingConfiguration();
+            this.logTarget = new MemoryTarget("memory_target");
+            loggingConfig.AddTarget(this.logTarget);
+            loggingConfig.AddRuleForAllLevels(this.logTarget);
+
+            // Setup Dependency Injection container to provide logger and out class under test (the "subject")
+            this.serviceProvider = new ServiceCollection()
+             .AddLogging(loggingBuilder =>
+             {
+                 // configure Logging with NLog
+                 loggingBuilder.ClearProviders();
+                 loggingBuilder.SetMinimumLevel(LogLevel.Trace);
+                 loggingBuilder.AddNLog(loggingConfig);
+             })
+             .BuildServiceProvider();
+
+            this.pcaWrapperMock = new Mock<IPCAWrapper>();
+
+            this.logger = this.serviceProvider.GetService<ILogger<AuthFlowFactory>>();
+            this.authMode = AuthMode.Broker;
+            this.scopes = new[] { $"{ResourceId}/.default" };
+            this.osxKeyChainSuffix = "azureauth";
+            this.preferredDomain = "contoso.com";
+            this.pcaWrapper = this.pcaWrapperMock.Object;
+            this.promptHint = "Log into Contoso!";
+        }
+
         [Test]
         public void JustBroker()
         {
-            IEnumerable<IAuthFlow> subject = AuthFlowFactory.Something(AuthMode.Broker);
+            IEnumerable<IAuthFlow> subject = AuthFlowFactory.Create(
+                this.logger,
+                this.authMode,
+                ClientId,
+                TenantId,
+                this.scopes,
+                this.osxKeyChainSuffix,
+                this.preferredDomain,
+                this.pcaWrapper,
+                this.promptHint);
 
             subject.Should().HaveCount(1);
             subject.First().GetType().Name.Should().Be(typeof(Broker).Name);
--- src/MSALWrapper/AuthFlow/AuthFlowFactory.cs
+++ src/MSALWrapper/AuthFlow/AuthFlowFactory.cs
@@ -5,6 +5,7 @@ namespace Microsoft.Authentication.MSALWrapper.AuthFlow
 {
     using System;
     using System.Collections.Generic;
+    using Microsoft.Extensions.Logging;
 
     /// <summary>
     /// A static class for creating an enumeration of auth flows given settings.
@@ -14,11 +15,31 @@ namespace Microsoft.Authentication.MSALWrapper.AuthFlow
         /// <summary>
         /// Create a list of <see cref="IAuthFlow"/> instances based on the given settings.
         /// </summary>
+        /// <param name="logger">An <see cref="ILogger"/>.</param>
         /// <param name="authMode">The desired <see cref="AuthMode"/>.</param>
+        /// <param name="clientId">The client id.</param>
+        /// <param name="tenantId">The tenant id.</param>
+        /// <param name="scopes">The scopes.</param>
+        /// <param name="osxKeyChainSuffix">A suffix to customize the OSX msal cache.</param>
+        /// <param name="preferredDomain">Preferred domain to use when filtering cached accounts.</param>
+        /// <param name="pcaWrapper">An injected PCAWrapper to use.</param>
+        /// <param name="promptHint">A prompt hint to contextualize an auth prompt if given.</param>
         /// <returns>An <see cref="IEnumerable{T}"/> of <see cref="IAuthFlow"/> instances.</returns>
-        public static IEnumerable<IAuthFlow> Something(AuthMode authMode)
+        public static IEnumerable<IAuthFlow> Create(
+            ILogger logger,
+            AuthMode authMode,
+            Guid clientId,
+            Guid tenantId,
+            IEnumerable<string> scopes,
+            string osxKeyChainSuffix,
+            string preferredDomain,
+            IPCAWrapper pcaWrapper,
+            string promptHint)
         {
-            throw new NotImplementedException();
+            return new[]
+            {
+                new Broker(logger, clientId, tenantId, scopes, osxKeyChainSuffix, preferredDomain, pcaWrapper, promptHint),
+            };
         }
     }
 }
