0506ec38eb8fb7ce1d2de090ea57d03af58ee18d
Rick Tang <ruitang@microsoft.com>
2022-05-11 15:49:46 -0700

    Remove unused using.

--- src/MSALWrapper.Test/AuthFlow/AuthFlowExecutorTest.cs
+++ src/MSALWrapper.Test/AuthFlow/AuthFlowExecutorTest.cs
@@ -5,7 +5,6 @@ namespace Microsoft.Authentication.MSALWrapper.Test
 {
     using System;
     using System.Collections.Generic;
-    using System.Linq;
     using System.Threading;
     using System.Threading.Tasks;
     using FluentAssertions;
