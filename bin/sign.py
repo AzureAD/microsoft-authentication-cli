d68cab2916aa627a2fe38f9799ce5029d83688ab
rsiemens <rsiemens@microsoft.com>
2022-05-20 12:16:19 -0700

    Add a MacOS batchmaker to sign.py

--- bin/sign.py
+++ bin/sign.py
@@ -11,8 +11,27 @@ from contextlib import ExitStack
 from contextlib import contextmanager
 from pathlib import Path
 from typing import Any
+from zipfile import ZipFile
+from zipfile import ZIP_DEFLATED
 
-JSON = dict[str, Any]  # A  naive type alias for JSON.
+JSON = dict[str, Any]  # A naive type alias for JSON.
+
+
+def mac_app_developer_sign(key_code: str) -> JSON:
+    """Return the JSON for a `MacAppDeveloperSign` operation."""
+    return {
+        "KeyCode": key_code,
+        "OperationCode": "MacAppDeveloperSign",
+        "Parameters": {
+            "OpusName": "Microsoft",
+            "OpusInfo": "http://www.microsoft.com",
+            "FileDigest": '/fd "SHA256"',
+            "PageHash": "/NPH",
+            "TimeStamp": '/tr "http://rfc3161.gtm.corp.microsoft.com/TSS/HttpTspServer" /td sha256',
+        },
+        "ToolName": "sign",
+        "ToolVersion": "1.0",
+    }
 
 
 def sign_tool_sign(key_code: str) -> JSON:
@@ -80,23 +99,65 @@ def windows_batches(
     key_code: str,
     customer_correlation_id: str,
 ) -> Generator[JSON, None, None]:
-    """Return the JSON signing batches for the Windows platform."""
-    extensions = [".exe", ".dll"]
+    """Yield the JSON signing batches for the win-x64 runtime."""
     files = [
         sign_request_file(path, path, customer_correlation_id)
         for path in source.iterdir()
-        if path.suffix in extensions and path.is_file()
+        if path.suffix in [".exe", ".dll"] and path.is_file()
     ]
 
-    operations = [
-        sign_tool_sign(key_code),
-        sign_tool_verify(key_code),
-    ]
+    operations = [sign_tool_sign(key_code), sign_tool_verify(key_code)]
 
+    # Yield the batches to ESRPClient.exe signing.
     yield {
         "Version": "1.0.0",
         "SignBatches": [batch(source, destination, files, operations)],
     }
+
+
+@contextmanager
+def osx_batches(
+    source: Path,
+    destination: Path,
+    key_code: str,
+    customer_correlation_id: str,
+) -> Generator[JSON, None, None]:
+    """Yield the JSON signing batches for the osx-x64 and osx-arm64 runtimes."""
+    dlls = []
+    dylibs = []
+    dylibs_zip = source / "dylibs.zip"
+
+    # Find .dlls and .dylibs (including azureauth).
+    for path in source.iterdir():
+        if path.suffix == ".dll" and path.is_file():
+            dlls.append(path)
+        elif (path.name == "azureauth" or path.suffix == ".dylib") and path.is_file():
+            dylibs.append(path)
+
+    with ZipFile(dylibs_zip, mode="w", compression=ZIP_DEFLATED) as file:
+        for path in dylibs:
+            file.write(path, path.relative_to(source))
+
+    dll_files = [sign_request_file(dll, dll, customer_correlation_id) for dll in dlls]
+    dylib_files = [sign_request_file(dylibs_zip, dylibs_zip, customer_correlation_id)]
+
+    dll_operations = [sign_tool_sign(key_code), sign_tool_verify(key_code)]
+    dylib_operations = [mac_app_developer_sign(key_code)]
+
+    # Yield the batches to ESRPClient.exe signing.
+    yield {
+        "Version": "1.0.0",
+        "SignBatches": [
+            batch(source, destination, dll_files, dll_operations),
+            batch(source, destination, dylib_files, dylib_operations),
+        ],
+    }
+
+    # At this point signing is finished. Extract the signed dylibs.
+    dylibs_zip = destination / dylibs_zip.name
+    with ZipFile(dylibs_zip, mode="r") as file:
+        file.extractall(destination)
+    dylibs_zip.unlink()
 
 
 def auth(tenant_id: str, client_id: str) -> JSON:
@@ -145,13 +206,17 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument("esrp_client")
     parser.add_argument("--source", default=str(cwd))
     parser.add_argument("--destination", default=str(cwd))
-    parser.add_argument("--platform", choices=["windows", "macos"], default="windows")
+    parser.add_argument(
+        "--runtime",
+        choices=["win-x64", "osx-x64", "osx-arm64"],
+        default="win-x64",
+    )
 
     return parser.parse_args()
 
 
 def main() -> None:
-    """Determine target platform, generate inputs, and run ESRPClient.exe."""
+    """Determine target runtime, generate inputs, and run ESRPClient.exe."""
     # 1. Parse command line arguments.
     args = parse_args()
 
@@ -173,23 +238,25 @@ def main() -> None:
     input_path = Path("input.json")
     output_path = Path("output.json")
 
-    # 3. Determine platform & create a batchmaker.
-    # 
-    # Note that the batchmaker is not the batches themselves, but a context
-    # manager which will yield them.
-    match args.platform.lower():
-        case "windows":
+    # 3. Determine runtime & create a batchmaker.
+    match args.runtime.lower():
+        case "win-x64":
             batchmaker = windows_batches(
                 source=source_path,
                 destination=destination_path,
                 key_code=key_code,
                 customer_correlation_id=customer_correlation_id,
             )
-        case "macos":
-            sys.exit("Error: Platform 'macos' not yet implemented!")
+        case "osx-x64" | "osx-arm64":
+            batchmaker = osx_batches(
+                source=source_path,
+                destination=destination_path,
+                key_code=key_code,
+                customer_correlation_id=customer_correlation_id,
+            )
         case _:
             # This should be unreachable because of argparse, but let's be safe.
-            sys.exit(f"Error: Invalid platform: {args.platform}")
+            sys.exit(f"Error: Invalid runtime: {args.runtime}")
 
     # 4. Create the necessary context and run ESRPClient.
     esrp_args = [
@@ -207,6 +274,7 @@ def main() -> None:
         "Verbose",
     ]
 
+    # All temporary files created in this context should be cleaned up.
     with ExitStack() as stack:
         # Generate auth.json.
         auth_json = auth(tenant_id, aad_id)
@@ -220,8 +288,8 @@ def main() -> None:
         batches = stack.enter_context(batchmaker)
         stack.enter_context(json_tempfile(input_path, batches))
 
-        # subprocess.run(esrp_args)
-        print(f"Running {esrp_args}")
+        # Run ESRPClient.exe.
+        subprocess.run(esrp_args)
 
 
 if __name__ == "__main__":
