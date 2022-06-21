e164b7db581f644da3d9129bb97fb8d9c9a4438d
Reilly Tucker Siemens <rsiemens@microsoft.com>
2022-06-20 17:50:18 -0700

    Modify Unix install script for post-0.4.0 install

--- install/install.sh
+++ install/install.sh
@@ -50,55 +50,139 @@ release_name() {
     echo "${name}"
 }
 
-: ${AZUREAUTH_REPO='AzureAD/microsoft-authentication-cli'}
-repo="${AZUREAUTH_REPO}"
-release_file="$(release_name).tar.gz"
-release_url="https://github.com/${repo}/releases/download/${version}/${release_file}"
+remove_from_path() {
+    path="${1}"
+    shell_profile="${2}"
 
-: ${AZUREAUTH_INSTALL_DIRECTORY="${HOME}/.azureauth"}
-azureauth_directory="${AZUREAUTH_INSTALL_DIRECTORY}"
-target_directory="${azureauth_directory}/$(release_name)"
-latest_directory="${azureauth_directory}/latest"
-tarball="${azureauth_directory}/${release_file}"
-
-verbose "Creating ${azureauth_directory}"
-mkdir -p $azureauth_directory
-
-verbose "Downloading ${release_url} to ${tarball}"
-if ! curl -sfL $release_url > $tarball; then
-    error "Failed to download azureauth ${version}"
-    exit 1
-fi
-
-if [ -d "${target_directory}" ]; then
-    verbose "Removing pre-existing target directory at ${target_directory}"
-    rm -rf $target_directory
-fi
-
-verbose "Extracting ${tarball} to ${target_directory}"
-mkdir -p $target_directory
-tar -xf $tarball -C $target_directory
-
-# The files extracted from the tarball are all executable, but only two need to be.
-chmod -x ${target_directory}/*
-chmod +x ${target_directory}/azureauth
-
-verbose "Removing ${tarball}"
-rm $tarball
-
-verbose "Linking ${latest_directory} to ${target_directory}"
-# It's very important we use -n and -f here or the symlink won't actually be overwritten during upgrades.
-ln -snf $target_directory $latest_directory
-
-# We currently only support automatically appending $PATH modifications for the default
-# Bash and ZSH profiles as the syntax is identical.
-path_modification='export PATH="${PATH}:${HOME}/.azureauth/latest"'
-for shell_profile in "${HOME}/.bashrc" "${HOME}/.zshrc"
-do
-    if ! grep "${path_modification}" "${shell_profile}" &>/dev/null; then
-        verbose "Updating \$PATH in ${shell_profile} to include ${latest_directory}"
-        printf "\n${path_modification}\n" >> $shell_profile
+    if grep "${path}" "${shell_profile}" &>/dev/null; then
+        path_suffix=$(echo "${path}" | awk -F : '{print $NF}' | tr -d '"')
+        verbose "Removing '${path_suffix}' from \$PATH in ${shell_profile}"
+        escaped_path=$(echo "${path}" | sed -e 's;/;\\/;g')
+        sed -i -e /"${escaped_path}"/d "${shell_profile}"
     fi
-done
+}
 
-echo "Installed azureauth ${version}!"
+install_pre_0_4_0() {
+    : ${AZUREAUTH_REPO='AzureAD/microsoft-authentication-cli'}
+    repo="${AZUREAUTH_REPO}"
+    release_file="$(release_name).tar.gz"
+    release_url="https://github.com/${repo}/releases/download/${version}/${release_file}"
+
+    : ${AZUREAUTH_INSTALL_DIRECTORY="${HOME}/.azureauth"}
+    azureauth_directory="${AZUREAUTH_INSTALL_DIRECTORY}"
+    target_directory="${azureauth_directory}/$(release_name)"
+    latest_directory="${azureauth_directory}/latest"
+    tarball="${azureauth_directory}/${release_file}"
+
+    verbose "Creating ${azureauth_directory}"
+    mkdir -p $azureauth_directory
+
+    verbose "Downloading ${release_url} to ${tarball}"
+    if ! curl -sfL $release_url > $tarball; then
+        error "Failed to download azureauth ${version}"
+        exit 1
+    fi
+
+    if [ -d "${target_directory}" ]; then
+        verbose "Removing pre-existing target directory at ${target_directory}"
+        rm -rf $target_directory
+    fi
+
+    verbose "Extracting ${tarball} to ${target_directory}"
+    mkdir -p $target_directory
+    tar -xf $tarball -C $target_directory
+
+    # The files extracted from the tarball are all executable, but only two need to be.
+    chmod -x ${target_directory}/*
+    chmod +x ${target_directory}/azureauth
+
+    verbose "Removing ${tarball}"
+    rm $tarball
+
+    verbose "Linking ${latest_directory} to ${target_directory}"
+    # It's very important we use -n and -f here or the symlink won't actually be overwritten during upgrades.
+    ln -snf $target_directory $latest_directory
+
+    # We currently only support automatically appending $PATH modifications for the default
+    # Bash and ZSH profiles as the syntax is identical.
+    path_modification='export PATH="${PATH}:${HOME}/.azureauth/latest"'
+    for shell_profile in "${HOME}/.bashrc" "${HOME}/.zshrc"
+    do
+        if ! grep "${path_modification}" "${shell_profile}" &>/dev/null; then
+            verbose "Updating \$PATH in ${shell_profile} to include ${latest_directory}"
+            printf "\n${path_modification}\n" >> $shell_profile
+        fi
+    done
+
+    echo "Installed azureauth ${version}!"
+}
+
+install_post_0_4_0() {
+    : ${AZUREAUTH_REPO='AzureAD/microsoft-authentication-cli'}
+    repo="${AZUREAUTH_REPO}"
+    release_file="$(release_name).tar.gz"
+    release_url="https://github.com/${repo}/releases/download/${version}/${release_file}"
+
+    : ${AZUREAUTH_INSTALL_DIRECTORY="${HOME}/.azureauth"}
+    azureauth_directory="${AZUREAUTH_INSTALL_DIRECTORY}"
+    target_directory="${azureauth_directory}/${version}"
+    tarball="${azureauth_directory}/${release_file}"
+
+    verbose "Creating ${azureauth_directory}"
+    mkdir -p $azureauth_directory
+
+    verbose "Downloading ${release_url} to ${tarball}"
+    if ! curl -sfL $release_url > $tarball; then
+        error "Failed to download azureauth ${version}"
+        exit 1
+    fi
+
+    if [ -d "${target_directory}" ]; then
+        verbose "Removing pre-existing target directory at ${target_directory}"
+        rm -rf $target_directory
+    fi
+
+    verbose "Extracting ${tarball} to ${target_directory}"
+    mkdir -p $target_directory
+    tar -xf $tarball -C $target_directory
+
+    verbose "Removing ${tarball}"
+    rm $tarball
+
+    # We previously added `latest` to the $PATH, but we no longer support that, so
+    # we remove this from the $PATH to avoid confusion.
+    latest_path='export PATH="${PATH}:${HOME}/.azureauth/latest"'
+
+    # If there is an existing installation we can identify, then we remove it from
+    # the $PATH so that it will be replaced by the new installation.
+    current_azureauth="$(which azureauth)"
+    if [ -f "${current_azureauth}" ]; then
+        current_azureauth_parent=$(dirname "${current_azureauth}")
+        current_path='export PATH="${PATH}:'${current_azureauth_parent}'"'
+    fi
+
+    for shell_profile in "${HOME}/.bashrc" "${HOME}/.zshrc"
+    do
+        remove_from_path "${latest_path}" "${shell_profile}"
+        if [ -z "${current_path}" ]; then
+            remove_from_path "${current_path}" "${shell_profile}"
+        fi
+    done
+
+    # We currently only support automatically appending $PATH modifications for the default
+    # Bash and ZSH profiles as the syntax is identical.
+    new_path='export PATH="${PATH}:'${target_directory}'"'
+    for shell_profile in "${HOME}/.bashrc" "${HOME}/.zshrc"
+    do
+        if ! grep "${new_path}" "${shell_profile}" &>/dev/null; then
+            verbose "Appending '${target_directory} to \$PATH in ${shell_profile}"
+            printf "${new_path}\n" >> $shell_profile
+        fi
+    done
+
+    echo "Installed azureauth ${version}!"
+}
+
+# TODO: Version determination logic.
+# install_pre_0_4_0
+install_post_0_4_0
