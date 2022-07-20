4876e079c662eb9729b4b3992a0ea286e76c71db
mvanchaa <manuhavancha@microsoft.com>
2022-07-20 10:42:57 -0700

    Add a note on the corext env variable

--- src/AzureAuth/EnvVars.cs
+++ src/AzureAuth/EnvVars.cs
@@ -29,9 +29,9 @@ namespace Microsoft.Authentication.AzureAuth
         public static readonly string DisableInteractivity = $"{EnvVarPrefix}_DISABLE_INTERACTIVITY";
 
         /// <summary>
-        /// Name of the env var used to disable user based authentication modes. This serves the same purpose as the env variable `DisableUserAuthModes`.
+        /// Name of the env var used to disable user based authentication modes. NOTE: This is a private variable and it is recommended to not rely on this variable.
         /// </summary>
-        public static readonly string CorextNonInteractive = $"Corext_NonInteractive";
+        internal static readonly string CorextNonInteractive = $"Corext_NonInteractive";
 
         private const string EnvVarPrefix = "AZUREAUTH";
     }
