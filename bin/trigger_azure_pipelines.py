80370ad494f311b117eeadbbd8cf66e7e39ea5ba
Manuha Vancha <manuhavancha@microsoft.com>
2022-12-12 06:05:41 -0800

    Pass template parameters when triggering a build

--- bin/trigger_azure_pipelines.py
+++ bin/trigger_azure_pipelines.py
@@ -8,13 +8,12 @@ import sys
 import time
 
 from azure.devops.connection import Connection
-from azure.devops.released.build.build_client import BuildClient
-from azure.devops.v5_1.build.models import Build
+from azure.devops.v6_0.pipelines.pipelines_client import PipelinesClient
+from azure.devops.v6_0.pipelines.models import Run
 from msrest.authentication import BasicAuthentication
 
-# https://learn.microsoft.com/en-us/rest/api/azure/devops/build/builds/get?view=azure-devops-rest-7.1#buildresult
-FAILED_STATUSES: set[str] = {"canceled", "partiallySucceeded", "failed"}
-COMPLETED_STATUSES: set[str] = FAILED_STATUSES | {"succeeded"}
+# https://learn.microsoft.com/en-us/rest/api/azure/devops/pipelines/runs/get?view=azure-devops-rest-6.0#runresult
+FAILED_STATUSES: set[str] = {"canceled", "failed", "unknown"}
 
 
 def ado_connection(organization: str, ado_pat: str) -> Connection:
@@ -26,43 +25,51 @@ def ado_connection(organization: str, ado_pat: str) -> Connection:
     )
 
 
-def wait_for_build(build_client: BuildClient, project: str, build_id: str) -> Build:
-    """Wait for the azure devops build to finish"""
-    build = build_client.get_build(project, build_id)
+def wait_for_pipeline_run(pipeline_client: PipelinesClient, project: str, pipeline_id: int, run_id: str) -> Run:
+    """Wait for the azure devops pipepline run to finish"""
+    run = pipeline_client.get_run(project, pipeline_id, run_id)
 
     # polling interval is set in accordance with the rate limits specified here:
     # https://learn.microsoft.com/en-us/azure/devops/integrate/concepts/rate-limits?view=azure-devops
     polling_interval_seconds = 30
 
     # Wait until the build have a complete status.
-    while build.result not in COMPLETED_STATUSES:
+    # https://learn.microsoft.com/en-us/rest/api/azure/devops/pipelines/runs/get?view=azure-devops-rest-6.0#runstate
+    while run.state != "completed":
         time.sleep(polling_interval_seconds)
-        build = build_client.get_build(project, build_id)
-    return build
+        run = pipeline_client.get_run(project, pipeline_id, run_id)
+    return run
 
 
 def trigger_azure_pipeline_and_wait_until_its_completed(
     organization: str,
     project: str,
-    pipeline_id: str,
+    pipeline_id: int,
     ado_pat: str,
+    version: str,
+    commitSHA: str
 ) -> None:
     """Triggers an azure pipeline and waits for it to be finished"""
-    build_client = ado_connection(organization, ado_pat).clients.get_build_client()
-    build_metadata = {"definition": {"id": pipeline_id}}
+    ado_client = ado_connection(organization, ado_pat).clients_v6_0
+    pipeline_client = ado_client.get_pipelines_client()
+    
+    # NOTE: We run a pipeline instead of queuing a build because only the run pipeline API allows us to pass template parameters and build API doesn't support it.
+    # run pipeline API: https://learn.microsoft.com/en-us/rest/api/azure/devops/pipelines/runs/run-pipeline?view=azure-devops-rest-6.0 
+    # queue build API: https://learn.microsoft.com/en-us/rest/api/azure/devops/build/builds/queue?view=azure-devops-rest-6.0
 
-    triggered_build = build_client.queue_build(build_metadata, project)
-    build_url = f"https://dev.azure.com/{organization}/{project}/_build/results?buildId={triggered_build.id}&view=results"
+    # run_parameters = {"templateParameters": {"version": version, "commitSHA": commitSHA}}
+    run_parameters = {"templateParameters": {"upstream_version": version, "commitSHA": commitSHA}, 
+        "resources": {"repositories": {"self": {"refName": "refs/heads/debian_scripts"}}}}
+
+    pipeline_status = pipeline_client.run_pipeline(run_parameters, project, pipeline_id)
+    pipeline_url = f"https://dev.azure.com/{organization}/{project}/_build/results?buildId={pipeline_status.id}&view=results"
     print(
-        "Successfully triggered a build. Waiting for the build to be completed.\n"
-        f"More details on the build can be found here: {build_url}"
+        "Successfully triggered a pipeline. Waiting for the run to be completed.\n"
+        f"More details on the triggered pipeline can be found here: {pipeline_url}"
     )
-
-    completed_build = wait_for_build(build_client, project, triggered_build.id)
-    if completed_build.result in FAILED_STATUSES:
-        raise Exception("Azure DevOps build failed!")
-
-    print("Azure DevOps build succeeded!")
+    completed_run = wait_for_pipeline_run(pipeline_client, project, pipeline_id, pipeline_status.id)
+    if completed_run.result in FAILED_STATUSES:
+        raise Exception("Azure DevOps pipeline run failed!")
 
 
 def main() -> None:
@@ -74,7 +81,8 @@ def main() -> None:
         organization = os.environ["ADO_ORGANIZATION"]
         project = os.environ["ADO_PROJECT"]
         pipeline_id = os.environ["ADO_AZUREAUTH_LINUX_PIPELINE_ID"]
-
+        version = os.environ["VERSION"]
+        commitSHA = os.environ["commitSHA"]
     except KeyError as exc:
         # See https://stackoverflow.com/a/24999035/3288364.
         name = str(exc).replace("'", "")
@@ -86,6 +94,8 @@ def main() -> None:
         project,
         pipeline_id,
         ado_pat,
+        version,
+        commitSHA
     )
 
 
