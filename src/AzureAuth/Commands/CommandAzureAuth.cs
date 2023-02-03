01456874177a950120bae5158b5aa8855a718733
Kyle Rader <kyrader@microsoft.com>
2023-02-03 13:09:59 -0800

    add main subcommands

--- src/AzureAuth/Commands/CommandAzureAuth.cs
+++ src/AzureAuth/Commands/CommandAzureAuth.cs
@@ -9,6 +9,7 @@ namespace Microsoft.Authentication.AzureAuth.Commands
     /// The command main class parses commands and dispatches to the corresponding methods.
     /// </summary>
     [Command(Name = "azureauth", Description = "A CLI interface to MSAL (Microsoft Authentication Library)")]
+    [Subcommand(typeof(CommandAad), typeof(CommandAdo))]
     public class CommandAzureAuth
     {
         /// <summary>
