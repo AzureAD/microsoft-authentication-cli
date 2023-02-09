518e51c4cd2b0471e04cd7fabadc09b221fbf6a1
Kyle Rader <kyrader@microsoft.com>
2023-02-09 13:41:49 -0800

    Add Ado constants file

---
+++ src/AzureAuth/Ado/Constants.cs
@@ -0,0 +1,33 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace Microsoft.Authentication.AzureAuth.Ado
+{
+    /// <summary>
+    /// Azure Devops constant values.
+    /// </summary>
+    internal static class Constants
+    {
+        /// <summary>
+        /// App Registration Client IDs.
+        /// </summary>
+        public static class Client
+        {
+            /// <summary>
+            /// Visual Studio 2019 and earlier client ID.
+            /// </summary>
+            public static string VisualStudio = "872cd9fa-d31f-45e0-9eab-6e460a02d1f1";
+        }
+
+        /// <summary>
+        /// Resource Scopes.
+        /// </summary>
+        public static class Scope
+        {
+            /// <summary>
+            /// The default scope used for Azure Devops.
+            /// </summary>
+            public static string AzureDevopsDefault = "499b84ac-1321-427f-aa17-267ca6975798/.default";
+        }
+    }
+}
