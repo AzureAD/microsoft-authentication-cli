82b2e800621dcb8ec1b1d57e25d00b98bf758d97
Reilly Tucker Siemens <rsiemens@microsoft.com>
2023-03-02 11:57:32 -0800

    Add unlit PatManager and supporting interfaces (#212)
    * Implement PatManager and supporting interfaces
    
    * Declare token type in out parameter
    
    * Note that PatClient, PatCache, and PatManager are not threadsafe
    
    * Use string.Join to construct PatCache key
    
    * Rename PatCache Get and Put methods
    
    * Use DateTime.UnixEpoch everywhere
    
    * Remove "Pat" from PatClient method names
    
    * Prefer CacheKey to ToString
    
    * Use else if instead of checking all PAT expirations
    
    * Make PatClient.CreateAsync use primitives instead of PatTokenCreateRequest
    
    * Add a comment describing PAT cache scenarios
    
    * Clarify cache ownership in comments

---
+++ src/AdoPat/PatManager.cs
@@ -0,0 +1,93 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace Microsoft.Authentication.AdoPat
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.VisualStudio.Services.DelegatedAuthorization;
+
+    /// <summary>
+    /// Manage the interaction between the PAT cache and Azure DevOps.
+    /// </summary>
+    /// <remarks>
+    /// This class and its methods are not threadsafe. Locking must be handled externally.
+    /// </remarks>
+    public class PatManager
+    {
+        private const int ValidToExtensionDays = 7;
+        private const int NearingExpirationDays = 2;
+
+        private IPatCache cache;
+        private IPatClient client;
+        private Func<DateTime> now;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PatManager"/> class.
+        /// </summary>
+        /// <param name="cache">Any class that implements the <see cref="IPatCache"/> interface.</param>
+        /// <param name="client">Any class that implements the <see cref="IPatClient"/> interface.</param>
+        /// <param name="now">A function for computing the current moment. Defaults to null, which
+        /// uses <see cref="DateTime.UtcNow"/>. Overriding this should only be necessary in testing.</param>
+        public PatManager(IPatCache cache, IPatClient client, Func<DateTime> now = null)
+        {
+            this.cache = cache;
+            this.client = client;
+            this.now = now ?? new Func<DateTime>(() => DateTime.UtcNow);
+        }
+
+        /// <summary>
+        /// Fetches a PAT matching the given options from the cache, creating or regenerating a new PAT as necessary.
+        /// The cache controlled by this PatManager is not a shared cache and is *only* updated by this method.
+        /// </summary>
+        /// <param name="options">Options used to match the PAT in the cache or when creating/regenerating a new one.</param>
+        /// <param name="cancellationToken">The cancellation token to cancel the operation.</param>
+        /// <returns>An Azure DevOps Personal Access Token.</returns>
+        public async Task<PatToken> GetPatAsync(
+            PatOptions options,
+            CancellationToken cancellationToken = default)
+        {
+            var pat = this.cache.Get(options.CacheKey());
+
+            // If the PAT is null it means it wasn't present in the cache, so we must create one.
+            // If the PAT was present in the cache, but is inactive we must also create a new one.
+            // If the PAT was present in the cache, but will expire soon we must regenerate it.
+            // Otherwise we can simply return the PAT as is.
+            if (pat == null || await this.Inactive(pat, cancellationToken).ConfigureAwait(false))
+            {
+                pat = await this.client.CreateAsync(
+                    displayName: options.DisplayName,
+                    scope: string.Join(" ", options.Scopes),
+                    validTo: this.now().AddDays(ValidToExtensionDays),
+                    cancellationToken: cancellationToken)
+                .ConfigureAwait(false);
+                this.cache.Put(options.CacheKey(), pat);
+            }
+            else if (this.ExpiringSoon(pat))
+            {
+                pat = await this.client.RegenerateAsync(
+                    pat,
+                    this.now().AddDays(ValidToExtensionDays),
+                    cancellationToken)
+                .ConfigureAwait(false);
+                this.cache.Put(options.CacheKey(), pat);
+            }
+
+            return pat;
+        }
+
+        // Whether the given PAT is still considered active by Azure DevOps.
+        private async Task<bool> Inactive(PatToken pat, CancellationToken cancellationToken = default)
+        {
+            var activePats = await this.client.ListActiveAsync(cancellationToken).ConfigureAwait(false);
+            return !activePats.ContainsKey(pat.AuthorizationId);
+        }
+
+        // Whether the given PAT will expire before `NearingExpirationDays`.
+        private bool ExpiringSoon(PatToken pat)
+        {
+            return pat.ValidTo < this.now().AddDays(NearingExpirationDays);
+        }
+    }
+}
