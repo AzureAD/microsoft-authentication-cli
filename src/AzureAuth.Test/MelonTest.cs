1800e7aa8ff9bbafe6b0a2b3ccddaaf3f2619bbe
Kyle Rader <kyrader@microsoft.com>
2023-03-23 14:40:20 -0700

    Create Melon and MelonTest

---
+++ src/AzureAuth.Test/MelonTest.cs
@@ -0,0 +1,44 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace AzureAuth.Test
+{
+    using FluentAssertions;
+
+    using Microsoft.Authentication.AzureAuth;
+    using Microsoft.Authentication.MSALWrapper;
+    using Microsoft.Authentication.TestHelper;
+    using Microsoft.Extensions.Logging;
+    using Microsoft.Office.Lasso.Interfaces;
+
+    using Moq;
+
+    using NLog.Targets;
+    using NUnit.Framework;
+
+    internal class MelonTest
+    {
+        private ILogger logger;
+        private MemoryTarget logTarget;
+        private Mock<IEnv> env;
+        private Mock<ITelemetryService> telemetryService;
+        private Mock<ITokenFetcher> tokenFetcher;
+
+        [SetUp]
+        public void SetUp()
+        {
+            (this.logger, this.logTarget) = MemoryLogger.Create();
+            this.env = new Mock<IEnv>(MockBehavior.Strict);
+            this.telemetryService = new Mock<ITelemetryService>(MockBehavior.Strict);
+            this.tokenFetcher = new Mock<ITokenFetcher>(MockBehavior.Strict);
+        }
+
+        public Melon Subject() => new Melon(this.logger, this.env.Object, this.telemetryService.Object, this.tokenFetcher.Object);
+
+        [Test]
+        public void Contructor_Works()
+        {
+            this.Subject().Should().NotBeNull();
+        }
+    }
+}
---
+++ src/AzureAuth/Melon.cs
@@ -0,0 +1,28 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace Microsoft.Authentication.AzureAuth
+{
+    using Microsoft.Authentication.MSALWrapper;
+    using Microsoft.Extensions.Logging;
+    using Microsoft.Office.Lasso.Interfaces;
+
+    /// <summary>
+    /// A class for handling AAD Token acuisition, results logging, and telemetry collection.
+    /// </summary>
+    public class Melon
+    {
+        private readonly ILogger logger;
+        private readonly IEnv env;
+        private readonly ITelemetryService telemetryService;
+        private readonly ITokenFetcher tokenFetcher;
+
+        public Melon(ILogger logger, IEnv env, ITelemetryService telemetryService, ITokenFetcher tokenFetcher)
+        {
+            this.logger = logger;
+            this.env = env;
+            this.telemetryService = telemetryService;
+            this.tokenFetcher = tokenFetcher;
+        }
+    }
+}
