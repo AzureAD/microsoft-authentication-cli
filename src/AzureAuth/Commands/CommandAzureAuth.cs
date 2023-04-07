07f3ba20889e430248ac549d6d74b3fbf8d4c720
Kyle Rader <kyrader@microsoft.com>
2023-04-06 17:15:31 -0700

    Enable ado sub-command and remove root command

--- src/AzureAuth/Commands/CommandAzureAuth.cs
+++ src/AzureAuth/Commands/CommandAzureAuth.cs
@@ -1,34 +1,32 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+using System.IO.Abstractions;
+
+using McMaster.Extensions.CommandLineUtils;
+
+using Microsoft.Extensions.Logging;
+using Microsoft.Office.Lasso.Interfaces;
+using Microsoft.Office.Lasso.Telemetry;
+
 namespace Microsoft.Authentication.AzureAuth.Commands
 {
-    using System.IO.Abstractions;
-
-    using McMaster.Extensions.CommandLineUtils;
-
-    using Microsoft.Extensions.Logging;
-    using Microsoft.Office.Lasso.Interfaces;
-    using Microsoft.Office.Lasso.Telemetry;
-
     /// <summary>
     /// The command main class parses commands and dispatches to the corresponding methods.
     /// </summary>
-    [Command(Name = "azureauth", Description = "A CLI interface to MSAL (Microsoft Authentication Library)." +
-        "\n\n⚠️ NOTICE: Use `azureauth aad [options]`.\n⚠️ The top-level azureauth command is deprecated and will print help text soon." +
-        "\n\n\u001b[31m-- azureauth [options]\u001b[0m" +
-        "\n\u001b[32m++ azureauth aad [options]\u001b[0m\n")]
+    [Command(Name = "azureauth", Description = "A CLI interface to MSAL (Microsoft Authentication Library).")]
     [Subcommand(typeof(CommandAad))]
-    //[Subcommand(typeof(CommandAdo))] // TODO: Enable Ado commands
+    [Subcommand(typeof(CommandAdo))]
     [Subcommand(typeof(CommandInfo))]
-    public class CommandAzureAuth : CommandAad
+    public class CommandAzureAuth
     {
-#pragma warning disable SA1648 // inheritdoc should be used with inheriting class
-        /// <inheritdoc/>
-        public CommandAzureAuth(CommandExecuteEventData eventData, ITelemetryService telemetryService, ILogger<CommandAzureAuth> logger, IFileSystem fileSystem, IEnv env)
-#pragma warning restore SA1648 // inheritdoc should be used with inheriting class
-            : base(eventData, telemetryService, logger, fileSystem, env)
+        /// <summary> Execute the command. </summary>
+        /// <param name="app">The current command instance.</param>
+        /// <returns>0.</returns>
+        public int OnExecute(CommandLineApplication app)
         {
+            app.ShowHelp();
+            return 0;
         }
     }
 }
