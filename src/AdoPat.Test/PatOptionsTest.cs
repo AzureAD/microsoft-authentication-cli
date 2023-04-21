d4d1227d35ee70eab00be7968119161b5a5e5352
rsiemens <rsiemens@microsoft.com>
2023-04-21 10:29:07 -0700

    Fix failing tests

--- src/AdoPat.Test/PatOptionsTest.cs
+++ src/AdoPat.Test/PatOptionsTest.cs
@@ -9,7 +9,9 @@ namespace Microsoft.Authentication.AdoPat.Test
     public class PatOptionsTest
     {
         private const string Organization = "contoso";
+        private const string OrganizationHash = "11f0b04eda61baebd5646fde72b0058e9713c30e950d2f3457bbbc1c3c68b31a";
         private const string DisplayName = "Test PAT";
+        private const string DisplayNameHash = "c27c517aad4237d8e221d8f52a438cb24f7f8078582d6cb025c238a25b2460f7";
 
         [Test]
         public void CacheKey_Uses_Organization_DisplayName_And_Scopes()
@@ -20,7 +22,7 @@ namespace Microsoft.Authentication.AdoPat.Test
                 DisplayName = DisplayName,
                 Scopes = new[] { "test.scope.a", "test.scope.b", "test.scope.c" },
             };
-            var expected = "11f0b04eda61baebd5646fde72b0058e9713c30e950d2f3457bbbc1c3c68b31a-c27c517aad4237d8e221d8f52a438cb24f7f8078582d6cb025c238a25b2460f7-eb52c21ad04b684f72bb1cef51e7f4ca58ce5a753744123a5df8f4e1781f831d";
+            var expected = $"{OrganizationHash}-{DisplayNameHash}-eb52c21ad04b684f72bb1cef51e7f4ca58ce5a753744123a5df8f4e1781f831d";
 
             patOptions.CacheKey().Should().Be(expected);
         }
