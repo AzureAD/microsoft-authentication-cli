8dafb4bd584d3c3c7d7ad3bf80c0d4e3fec2825a
Kyle Rader <kyrader@microsoft.com>
2023-04-21 01:33:07 -0700

    update iwa

--- src/MSALWrapper/AuthFlow/IntegratedWindowsAuthentication.cs
+++ src/MSALWrapper/AuthFlow/IntegratedWindowsAuthentication.cs
@@ -17,7 +17,6 @@ namespace Microsoft.Authentication.MSALWrapper.AuthFlow
     public class IntegratedWindowsAuthentication : AuthFlowBase
     {
         private const string NameValue = "iwa";
-        private readonly ILogger logger;
         private readonly IEnumerable<string> scopes;
         private readonly string preferredDomain;
         private readonly IPCAWrapper pcaWrapper;
@@ -38,7 +37,6 @@ namespace Microsoft.Authentication.MSALWrapper.AuthFlow
         /// <param name="pcaWrapper">Optional: IPCAWrapper to use.</param>
         public IntegratedWindowsAuthentication(ILogger logger, Guid clientId, Guid tenantId, IEnumerable<string> scopes, string preferredDomain = null, IPCAWrapper pcaWrapper = null)
         {
-            this.errors = new List<Exception>();
             this.logger = logger;
             this.scopes = scopes;
             this.preferredDomain = preferredDomain;
@@ -52,22 +50,20 @@ namespace Microsoft.Authentication.MSALWrapper.AuthFlow
         protected override async Task<TokenResult> GetTokenInnerAsync()
         {
             IAccount account = await this.pcaWrapper.TryToGetCachedAccountAsync(this.preferredDomain);
-            TokenResult tokenResult = null;
+            TokenResult tokenResult = await CachedAuth.GetTokenAsync(
+                this.logger,
+                this.scopes,
+                account,
+                this.pcaWrapper,
+                this.errors);
+
+            if (tokenResult != null)
+            {
+                return tokenResult;
+            }
 
             try
             {
-                tokenResult = await CachedAuth.GetTokenAsync(
-                    this.logger,
-                    this.scopes,
-                    account,
-                    this.pcaWrapper,
-                    this.errors);
-
-                if (tokenResult != null)
-                {
-                    return tokenResult;
-                }
-
                 tokenResult = await TaskExecutor.CompleteWithin(
                     this.logger,
                     this.integratedWindowsAuthTimeout,
@@ -78,36 +74,18 @@ namespace Microsoft.Authentication.MSALWrapper.AuthFlow
                 // If IWA worked, it was 100% silent.
                 tokenResult.SetSilent();
             }
-            catch (MsalUiRequiredException ex)
+            catch (MsalUiRequiredException ex) when (
+                ex.Classification == UiRequiredExceptionClassification.BasicAction
+                && ex.Message.StartsWith("AADSTS50076", StringComparison.OrdinalIgnoreCase))
             {
-                this.errors.Add(ex);
-                this.logger.LogDebug(ex.Message);
-
-                if (ex.Classification == UiRequiredExceptionClassification.BasicAction
-                      && ex.Message.StartsWith("AADSTS50076", StringComparison.OrdinalIgnoreCase))
-                {
-                    this.logger.LogWarning("IWA failed, 2FA is required.");
-                    this.logger.LogWarning("IWA can pass this requirement if you log into Windows with either a Smart Card or Windows Hello.");
-                }
+                this.logger.LogWarning("IWA failed, 2FA is required.");
+                this.logger.LogWarning("IWA can pass this requirement if you log into Windows with either a Smart Card or Windows Hello.");
+                throw;
             }
-            catch (MsalServiceException ex)
+            catch (MsalClientException ex) when (ex.Message.Contains("WS-Trust endpoint not found"))
             {
-                this.logger.LogWarning($"MSAL Service Exception! (Not expected)\n{ex.Message}");
-                this.errors.Add(ex);
-            }
-            catch (MsalClientException ex)
-            {
-                this.logger.LogWarning($"Msal Client Exception! (Not expected)\n{ex.Message}");
-                this.errors.Add(ex);
-                if (ex.Message.Contains("WS-Trust endpoint not found"))
-                {
-                    this.logger.LogWarning($"IWA only works on Corp Net, please turn on VPN.");
-                }
-            }
-            catch (NullReferenceException ex)
-            {
-                this.logger.LogWarning($"Msal unexpected null reference! (Not Expected)\n{ex.Message}");
-                this.errors.Add(ex);
+                this.logger.LogWarning($"IWA only works on Corp Net, please turn on VPN.");
+                throw;
             }
 
             return tokenResult;
