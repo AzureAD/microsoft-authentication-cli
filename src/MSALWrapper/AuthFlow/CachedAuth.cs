cc3b0b182af569f5b748a45ba6daa134af5f14e0
Kyle Rader <kyrader@microsoft.com>
2023-04-21 02:24:28 -0700

    introduce CachedAuth as an AuthFlowBase

--- src/MSALWrapper/AuthFlow/CachedAuth.cs
+++ src/MSALWrapper/AuthFlow/CachedAuth.cs
@@ -5,6 +5,7 @@ namespace Microsoft.Authentication.MSALWrapper.AuthFlow
 {
     using System;
     using System.Collections.Generic;
+    using System.Threading;
     using System.Threading.Tasks;
 
     using Microsoft.Extensions.Logging;
@@ -13,9 +14,77 @@ namespace Microsoft.Authentication.MSALWrapper.AuthFlow
     /// <summary>
     /// This class provides an abstraction over how to use the MSAL Cached Auth (known as GetTokenSilent).
     /// </summary>
-    public static class CachedAuth
+    public class CachedAuth : AuthFlowBase
     {
+        private const string NameValue = "cache";
         private static readonly TimeSpan CachedAuthTimeout = TimeSpan.FromSeconds(30);
+        private readonly IEnumerable<string> scopes;
+        private readonly string preferredDomain;
+        private readonly IPCAWrapper pcaWrapper;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CachedAuth"/> class.
+        /// </summary>
+        /// <param name="logger">The logger.</param>
+        /// <param name="clientId">The client id.</param>
+        /// <param name="tenantId">The tenant id.</param>
+        /// <param name="scopes">The scopes.</param>
+        /// <param name="preferredDomain">The preferred domain.</param>
+        /// <param name="pcaWrapper">Optional: IPCAWrapper to use.</param>
+        public CachedAuth(ILogger logger, Guid clientId, Guid tenantId, IEnumerable<string> scopes, string preferredDomain = null, IPCAWrapper pcaWrapper = null)
+        {
+            this.logger = logger;
+            this.scopes = scopes;
+            this.preferredDomain = preferredDomain;
+            this.pcaWrapper = pcaWrapper ?? this.BuildPCAWrapper(clientId, tenantId);
+        }
+
+        /// <inheritdoc/>
+        protected override string Name() => NameValue;
+
+        /// <inheritdoc/>
+        protected override async Task<TokenResult> GetTokenInnerAsync()
+        {
+            IAccount account = await this.pcaWrapper.TryToGetCachedAccountAsync(this.preferredDomain);
+            if (account == null)
+            {
+                this.logger.LogDebug("No cached account found!");
+                return null;
+            }
+
+            this.logger.LogDebug($"Using cached account '{account.Username}'");
+
+            TokenResult tokenResult = await TaskExecutor.CompleteWithin(
+                this.logger,
+                CachedAuthTimeout,
+                "Get Token Silent",
+                (cancellationToken) => this.pcaWrapper.GetTokenSilentAsync(this.scopes, account, cancellationToken),
+                this.errors).ConfigureAwait(false);
+
+            tokenResult.SetSilent();
+
+            return tokenResult;
+        }
+
+        private IPCAWrapper BuildPCAWrapper(Guid clientId, Guid tenantId)
+        {
+            var clientBuilder =
+                PublicClientApplicationBuilder
+                .Create($"{clientId}")
+                .WithAuthority($"https://login.microsoftonline.com/{tenantId}")
+                .WithLogging(
+                    this.LogMSAL,
+                    Identity.Client.LogLevel.Verbose,
+                    enablePiiLogging: false,
+                    enableDefaultPlatformLogging: true);
+
+            return new PCAWrapper(this.logger, clientBuilder.Build(), this.errors, tenantId);
+        }
+
+        private void LogMSAL(Identity.Client.LogLevel level, string message, bool containsPii)
+        {
+            this.logger.LogTrace($"MSAL: {message}");
+        }
 
         /// <summary>
         /// Try to get a token silently.
