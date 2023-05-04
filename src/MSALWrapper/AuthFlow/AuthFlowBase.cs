48fe231b443df1b157cd824609e1e3eeeb0c78a7
Kyle Rader <kyrader@microsoft.com>
2023-05-04 10:59:04 -0700

    Use an abstract Property instead of a method

--- src/MSALWrapper/AuthFlow/AuthFlowBase.cs
+++ src/MSALWrapper/AuthFlow/AuthFlowBase.cs
@@ -3,12 +3,12 @@
 
 namespace Microsoft.Authentication.MSALWrapper.AuthFlow
 {
-    using Microsoft.Extensions.Logging;
-    using Microsoft.Identity.Client;
-
     using System;
     using System.Collections.Generic;
     using System.Threading.Tasks;
+
+    using Microsoft.Extensions.Logging;
+    using Microsoft.Identity.Client;
 
     /// <summary>
     /// An abstract class that implements the <see cref="IAuthFlow"/> interface
@@ -21,7 +21,6 @@ namespace Microsoft.Authentication.MSALWrapper.AuthFlow
 
         /// <summary>A <see cref="ILogger"/> to use for logging.</summary>
         protected ILogger logger;
-
         private static readonly HashSet<Type> ExceptionsToCatch = new HashSet<Type>()
         {
             typeof(MsalUiRequiredException),
@@ -30,6 +29,12 @@ namespace Microsoft.Authentication.MSALWrapper.AuthFlow
             typeof(MsalException),
             typeof(NullReferenceException),
         };
+
+        /// <summary>
+        /// Gets the name of this AuthFlow.
+        /// </summary>
+        /// <returns>The <see cref="string"/> representation of the authflow name.</returns>
+        protected abstract string Name { get; }
 
         /// <inheritdoc/>
         public async Task<AuthFlowResult> GetTokenAsync()
@@ -43,11 +48,11 @@ namespace Microsoft.Authentication.MSALWrapper.AuthFlow
             catch (Exception ex) when (ExceptionsToCatch.Contains(ex.GetType()))
             {
                 this.errors.Add(ex);
-                this.logger.LogDebug($"Exception caught during {this.Name()} token acquisition");
+                this.logger.LogDebug($"Exception caught during {this.Name} token acquisition");
                 this.logger.LogDebug(ex.Message);
             }
 
-            return new AuthFlowResult(result, this.errors, this.Name());
+            return new AuthFlowResult(result, this.errors, this.Name);
         }
 
         /// <summary>
@@ -57,11 +62,5 @@ namespace Microsoft.Authentication.MSALWrapper.AuthFlow
         /// <param name="account">The <see cref="IAccount"/> to attempt to use.</param>
         /// <returns>a tuple of <see cref="TokenResult"/> and <see cref="IList{Exception}"/>.</returns>
         protected abstract Task<TokenResult> GetTokenInnerAsync();
-
-        /// <summary>
-        /// The name of this AuthFlow.
-        /// </summary>
-        /// <returns>The <see cref="string"/> representation of the authflow name.</returns>
-        protected abstract string Name();
     }
 }
