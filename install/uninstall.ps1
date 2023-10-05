df7ea5a55205bf81dbd880f4fbbb10eab2876a60
Emilio Alvear Cantu <emilio.alvear@hotmail.com>
2023-10-05 15:16:59 -0700

    Changed order of functions

--- install/uninstall.ps1
+++ install/uninstall.ps1
@@ -6,60 +6,6 @@ $script:ErrorActionPreference='Stop'
 
 $azureauthDefaultLocation = ([System.IO.Path]::Combine($Env:LOCALAPPDATA, "Programs", "AzureAuth"))
 
-function Uninstall {
-    if (![string]::IsNullOrEmpty($Env:AZUREAUTH_INSTALL_DIRECTORY) `
-            -And $Env:AZUREAUTH_INSTALL_DIRECTORY -ne $azureauthDefaultLocation) {
-        Write-Warning "Ignoring AZUREAUTH_INSTALL_DIRECTORY environment variable. Uninstallation for custom locations is not supported."
-    }
-
-    Kill-Process
-
-    Remove-Directories
-    Remove-FromPath
-
-    Write-WarningForCustomLocations
-    Write-Output "Uninstalled AzureAuth!"
-}
-
-function Remove-Directories {
-    Remove-InstallationFolder $azureauthDefaultLocation
-}
-
-function Remove-InstallationFolder {
-    param ([string]$directory)
-
-    if (Test-Path -Path $directory) {
-        Write-Verbose "Removing installations at '${directory}'"
-        Remove-Item -Force -Recurse $directory
-    } else {
-        Write-Verbose "There were no installations found at '${directory}'"
-    }
-}
-
-function Remove-FromPath {
-    $registryPath = 'Registry::HKEY_CURRENT_USER\Environment'
-    $currentPath = (Get-ItemProperty -Path $registryPath -Name PATH -ErrorAction SilentlyContinue).Path
-
-    # Reconstruct the $PATH without any azureauth directories.
-    $updatedPath = "";
-    if (($null) -ne $currentPath) {
-        $paths = $currentPath.Split(";")
-        $pathArr = @()
-        ForEach($path in $paths){
-            if(!$path.Equals("") -And !$path.Contains($azureauthDefaultLocation)) {
-                $pathArr += "${path}"
-            }
-            elseif (!$path.Equals("")) {
-                Write-Verbose "Removing '${path}' from `$env:PATH"
-            }
-        }
-        $updatedPath = ($pathArr -join ";") + ";"
-    }
-
-    Set-ItemProperty -Path $registryPath -Name PATH -Value $updatedPath
-    Send-SettingChange
-}
-
 function Get-CustomLocationsFromPath {
     $customLocations = [System.Collections.ArrayList]@()
     $azureauthsInPath = (Get-Command -Name azureauth -ErrorAction SilentlyContinue -CommandType Application -All).Source
@@ -91,6 +37,59 @@ function Write-WarningForCustomLocations {
     }
 }
 
+# Send WM_SETTINGCHANGE after changing Environment variables.
+# Refer to https://gist.github.com/alphp/78fffb6d69e5bb863c76bbfc767effda
+function Send-SettingChange {
+    Add-Type -Namespace Win32 -Name NativeMethods -MemberDefinition @"
+    [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+    public static extern IntPtr SendMessageTimeout(IntPtr hWnd, uint Msg, UIntPtr wParam, string lParam, uint fuFlags, uint uTimeout, out UIntPtr lpdwResult);
+"@
+    $HWND_BROADCAST = [IntPtr] 0xffff;
+    $WM_SETTINGCHANGE = 0x1a;
+    $result = [UIntPtr]::Zero
+    
+    [void] ([Win32.Nativemethods]::SendMessageTimeout($HWND_BROADCAST, $WM_SETTINGCHANGE, [UIntPtr]::Zero, "Environment", 2, 5000, [ref] $result))
+}
+
+function Remove-FromPath {
+    $registryPath = 'Registry::HKEY_CURRENT_USER\Environment'
+    $currentPath = (Get-ItemProperty -Path $registryPath -Name PATH -ErrorAction SilentlyContinue).Path
+
+    # Reconstruct the $PATH without any azureauth directories.
+    $updatedPath = "";
+    if (($null) -ne $currentPath) {
+        $paths = $currentPath.Split(";")
+        $pathArr = @()
+        ForEach($path in $paths){
+            if(!$path.Equals("") -And !$path.Contains($azureauthDefaultLocation)) {
+                $pathArr += "${path}"
+            }
+            elseif (!$path.Equals("")) {
+                Write-Verbose "Removing '${path}' from `$env:PATH"
+            }
+        }
+        $updatedPath = ($pathArr -join ";") + ";"
+    }
+
+    Set-ItemProperty -Path $registryPath -Name PATH -Value $updatedPath
+    Send-SettingChange
+}
+
+function Remove-InstallationFolder {
+    param ([string]$directory)
+
+    if (Test-Path -Path $directory) {
+        Write-Verbose "Removing installations at '${directory}'"
+        Remove-Item -Force -Recurse $directory
+    } else {
+        Write-Verbose "There were no installations found at '${directory}'"
+    }
+}
+
+function Remove-Directories {
+    Remove-InstallationFolder $azureauthDefaultLocation
+}
+
 function Kill-Process {
     # Uninstall will fail if there are instances of AzureAuth running. 
     # We suppress taskkill output here because this is a best effort attempt and we don't want the user to see its output.
@@ -107,18 +106,19 @@ function Kill-Process {
     }
 }
 
-# Send WM_SETTINGCHANGE after changing Environment variables.
-# Refer to https://gist.github.com/alphp/78fffb6d69e5bb863c76bbfc767effda
-function Send-SettingChange {
-    Add-Type -Namespace Win32 -Name NativeMethods -MemberDefinition @"
-    [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
-    public static extern IntPtr SendMessageTimeout(IntPtr hWnd, uint Msg, UIntPtr wParam, string lParam, uint fuFlags, uint uTimeout, out UIntPtr lpdwResult);
-"@
-    $HWND_BROADCAST = [IntPtr] 0xffff;
-    $WM_SETTINGCHANGE = 0x1a;
-    $result = [UIntPtr]::Zero
-    
-    [void] ([Win32.Nativemethods]::SendMessageTimeout($HWND_BROADCAST, $WM_SETTINGCHANGE, [UIntPtr]::Zero, "Environment", 2, 5000, [ref] $result))
+function Uninstall {
+    if (![string]::IsNullOrEmpty($Env:AZUREAUTH_INSTALL_DIRECTORY) `
+            -And $Env:AZUREAUTH_INSTALL_DIRECTORY -ne $azureauthDefaultLocation) {
+        Write-Warning "Ignoring AZUREAUTH_INSTALL_DIRECTORY environment variable. Uninstallation for custom locations is not supported."
+    }
+
+    Kill-Process
+
+    Remove-Directories
+    Remove-FromPath
+
+    Write-WarningForCustomLocations
+    Write-Output "Uninstalled AzureAuth!"
 }
 
 Uninstall
