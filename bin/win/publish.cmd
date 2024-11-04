3390024a7b1de219e9fa56265d3c68ba8966699d
REDMOND\keyuxuan <keyuxuan@microsoft.com>
2024-11-04 15:01:50 -0800

    fix comments

--- bin/win/publish.cmd
+++ bin/win/publish.cmd
@@ -8,5 +8,4 @@ if exist %~dp0\dist (
     rmdir /S /Q %~dp0\dist
 )
 
-
 CALL %~dp0\dotnet.cmd publish %~dp0\..\..\src\AzureAuth\AzureAuth.csproj --self-contained true -r win-x64 -c release -o %~dp0\..\..\dist %*
